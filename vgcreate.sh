220be6872ae79b51eb19f47b3927cd004f24c438
Ali Ghaffaari <ali.ghaffaari@gmail.com>
2017-09-15 16:25:10 +0200

    Using PARALLEL variable instead of actual command
    to make it configurable.

--- vgcreate.sh
+++ vgcreate.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
+PARALLEL=parallel.pl
+
 if [ -z "$1" ]; then
 	echo "[ERROR] too few argument."
 	echo "Usage: $0 TEXT_FILE [TEXT_FILE ...]"
 	exit 1
 fi
 
-echo -n "$@" | parallel.pl -d" " 'protoc --encode=vg.Graph vg.proto < {} > .{.}.pb'
-echo -n "$@" | parallel.pl -d" " 'cat <( varint encode $(stat --printf="%s" .{.}.pb) ) .{.}.pb > .{.}.un.vg'
-echo -n "$@" | parallel.pl -d" " 'cat .{.}.un.vg >> .all.un.vg'
+echo -n "$@" | $PARALLEL -d" " 'protoc --encode=vg.Graph vg.proto < {} > .{.}.pb'
+echo -n "$@" | $PARALLEL -d" " 'cat <( varint encode $(stat --printf="%s" .{.}.pb) ) .{.}.pb > .{.}.un.vg'
+echo -n "$@" | $PARALLEL -d" " 'cat .{.}.un.vg >> .all.un.vg'
 cat <( varint encode "$#" ) .all.un.vg | gzip -c - > all.vg
 rm -f .*.pb .*.un.vg
